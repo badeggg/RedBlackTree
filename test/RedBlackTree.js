c7f86d5987f4d7dce1d1859f1c47795d0311991d
zhaoxuxu <zhaoxuxu@tal.com>
2021-10-29 18:49:20 +0800

    chore: adjust test suit order

--- test/RedBlackTree.js
+++ test/RedBlackTree.js
@@ -75,12 +75,12 @@ tap.test('_leftRotate case: x is root(x.parent is NIL), y.left is NIL', async ta
     tree._leftRotate(tree._contents.get(1));
 
     tap.equal(tree.root, tree._contents.get(2));
-    tap.equal(tree._contents.get(2).parent, tree.NIL);
-    tap.equal(tree._contents.get(2).right, tree.NIL);
-    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
     tap.equal(tree._contents.get(1).parent, tree._contents.get(2));
     tap.equal(tree._contents.get(1).left, tree.NIL);
     tap.equal(tree._contents.get(1).right, tree.NIL);
+    tap.equal(tree._contents.get(2).parent, tree.NIL);
+    tap.equal(tree._contents.get(2).right, tree.NIL);
+    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
 });
 
 tap.test('_leftRotate case: x is x.parent.left, y.left is not NIL', async tap => {
@@ -108,58 +108,58 @@ tap.test('_leftRotate case: x is x.parent.left, y.left is not NIL', async tap =>
     tree.insert(15);
 
     tap.equal(tree.root, tree._contents.get(11));
-    tap.equal(tree._contents.get(11).parent, tree.NIL);
-    tap.equal(tree._contents.get(11).left, tree._contents.get(2));
-    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
-    tap.equal(tree._contents.get(2).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
-    tap.equal(tree._contents.get(2).right, tree._contents.get(7));
-    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(14).left, tree.NIL);
-    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
     tap.equal(tree._contents.get(1).parent, tree._contents.get(2));
     tap.equal(tree._contents.get(1).left, tree.NIL);
     tap.equal(tree._contents.get(1).right, tree.NIL);
-    tap.equal(tree._contents.get(7).parent, tree._contents.get(2));
-    tap.equal(tree._contents.get(7).left, tree._contents.get(5));
-    tap.equal(tree._contents.get(7).right, tree._contents.get(8));
-    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
-    tap.equal(tree._contents.get(15).left, tree.NIL);
-    tap.equal(tree._contents.get(15).right, tree.NIL);
+    tap.equal(tree._contents.get(2).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
+    tap.equal(tree._contents.get(2).right, tree._contents.get(7));
     tap.equal(tree._contents.get(5).parent, tree._contents.get(7));
     tap.equal(tree._contents.get(5).left, tree.NIL);
     tap.equal(tree._contents.get(5).right, tree.NIL);
+    tap.equal(tree._contents.get(7).parent, tree._contents.get(2));
+    tap.equal(tree._contents.get(7).left, tree._contents.get(5));
+    tap.equal(tree._contents.get(7).right, tree._contents.get(8));
     tap.equal(tree._contents.get(8).parent, tree._contents.get(7));
     tap.equal(tree._contents.get(8).left, tree.NIL);
     tap.equal(tree._contents.get(8).right, tree.NIL);
+    tap.equal(tree._contents.get(11).parent, tree.NIL);
+    tap.equal(tree._contents.get(11).left, tree._contents.get(2));
+    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
+    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(14).left, tree.NIL);
+    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
+    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
+    tap.equal(tree._contents.get(15).left, tree.NIL);
+    tap.equal(tree._contents.get(15).right, tree.NIL);
 
     tree._leftRotate(tree._contents.get(2));
 
     tap.equal(tree.root, tree._contents.get(11));
-    tap.equal(tree._contents.get(11).parent, tree.NIL);
-    tap.equal(tree._contents.get(11).left, tree._contents.get(7));
-    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
-    tap.equal(tree._contents.get(7).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(7).left, tree._contents.get(2));
-    tap.equal(tree._contents.get(7).right, tree._contents.get(8));
-    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(14).left, tree.NIL);
-    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
-    tap.equal(tree._contents.get(2).parent, tree._contents.get(7));
-    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
-    tap.equal(tree._contents.get(2).right, tree._contents.get(5));
-    tap.equal(tree._contents.get(8).parent, tree._contents.get(7));
-    tap.equal(tree._contents.get(8).left, tree.NIL);
-    tap.equal(tree._contents.get(8).right, tree.NIL);
-    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
-    tap.equal(tree._contents.get(15).left, tree.NIL);
-    tap.equal(tree._contents.get(15).right, tree.NIL);
     tap.equal(tree._contents.get(1).parent, tree._contents.get(2));
     tap.equal(tree._contents.get(1).left, tree.NIL);
     tap.equal(tree._contents.get(1).right, tree.NIL);
+    tap.equal(tree._contents.get(2).parent, tree._contents.get(7));
+    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
+    tap.equal(tree._contents.get(2).right, tree._contents.get(5));
     tap.equal(tree._contents.get(5).parent, tree._contents.get(2));
     tap.equal(tree._contents.get(5).left, tree.NIL);
     tap.equal(tree._contents.get(5).right, tree.NIL);
+    tap.equal(tree._contents.get(7).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(7).left, tree._contents.get(2));
+    tap.equal(tree._contents.get(7).right, tree._contents.get(8));
+    tap.equal(tree._contents.get(8).parent, tree._contents.get(7));
+    tap.equal(tree._contents.get(8).left, tree.NIL);
+    tap.equal(tree._contents.get(8).right, tree.NIL);
+    tap.equal(tree._contents.get(11).parent, tree.NIL);
+    tap.equal(tree._contents.get(11).left, tree._contents.get(7));
+    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
+    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(14).left, tree.NIL);
+    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
+    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
+    tap.equal(tree._contents.get(15).left, tree.NIL);
+    tap.equal(tree._contents.get(15).right, tree.NIL);
 });
 
 tap.test('_leftRotate case: x is x.parent.right, y.left is NIL', async tap => {
@@ -189,58 +189,58 @@ tap.test('_leftRotate case: x is x.parent.right, y.left is NIL', async tap => {
     tree.insert(15);
 
     tap.equal(tree.root, tree._contents.get(11));
-    tap.equal(tree._contents.get(11).parent, tree.NIL);
-    tap.equal(tree._contents.get(11).left, tree._contents.get(2));
-    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
-    tap.equal(tree._contents.get(2).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
-    tap.equal(tree._contents.get(2).right, tree._contents.get(7));
-    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(14).left, tree.NIL);
-    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
     tap.equal(tree._contents.get(1).parent, tree._contents.get(2));
     tap.equal(tree._contents.get(1).left, tree.NIL);
     tap.equal(tree._contents.get(1).right, tree.NIL);
-    tap.equal(tree._contents.get(7).parent, tree._contents.get(2));
-    tap.equal(tree._contents.get(7).left, tree._contents.get(5));
-    tap.equal(tree._contents.get(7).right, tree._contents.get(8));
-    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
-    tap.equal(tree._contents.get(15).left, tree.NIL);
-    tap.equal(tree._contents.get(15).right, tree.NIL);
+    tap.equal(tree._contents.get(2).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
+    tap.equal(tree._contents.get(2).right, tree._contents.get(7));
     tap.equal(tree._contents.get(5).parent, tree._contents.get(7));
     tap.equal(tree._contents.get(5).left, tree.NIL);
     tap.equal(tree._contents.get(5).right, tree.NIL);
+    tap.equal(tree._contents.get(7).parent, tree._contents.get(2));
+    tap.equal(tree._contents.get(7).left, tree._contents.get(5));
+    tap.equal(tree._contents.get(7).right, tree._contents.get(8));
     tap.equal(tree._contents.get(8).parent, tree._contents.get(7));
     tap.equal(tree._contents.get(8).left, tree.NIL);
     tap.equal(tree._contents.get(8).right, tree.NIL);
+    tap.equal(tree._contents.get(11).parent, tree.NIL);
+    tap.equal(tree._contents.get(11).left, tree._contents.get(2));
+    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
+    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(14).left, tree.NIL);
+    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
+    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
+    tap.equal(tree._contents.get(15).left, tree.NIL);
+    tap.equal(tree._contents.get(15).right, tree.NIL);
 
     tree._leftRotate(tree._contents.get(7));
 
     tap.equal(tree.root, tree._contents.get(11));
-    tap.equal(tree._contents.get(11).parent, tree.NIL);
-    tap.equal(tree._contents.get(11).left, tree._contents.get(2));
-    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
-    tap.equal(tree._contents.get(2).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
-    tap.equal(tree._contents.get(2).right, tree._contents.get(8));
-    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
-    tap.equal(tree._contents.get(14).left, tree.NIL);
-    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
     tap.equal(tree._contents.get(1).parent, tree._contents.get(2));
     tap.equal(tree._contents.get(1).left, tree.NIL);
     tap.equal(tree._contents.get(1).right, tree.NIL);
-    tap.equal(tree._contents.get(8).parent, tree._contents.get(2));
-    tap.equal(tree._contents.get(8).left, tree._contents.get(7));
-    tap.equal(tree._contents.get(8).right, tree.NIL);
-    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
-    tap.equal(tree._contents.get(15).left, tree.NIL);
-    tap.equal(tree._contents.get(15).right, tree.NIL);
-    tap.equal(tree._contents.get(7).parent, tree._contents.get(8));
-    tap.equal(tree._contents.get(7).left, tree._contents.get(5));
-    tap.equal(tree._contents.get(7).right, tree.NIL);
+    tap.equal(tree._contents.get(2).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(2).left, tree._contents.get(1));
+    tap.equal(tree._contents.get(2).right, tree._contents.get(8));
     tap.equal(tree._contents.get(5).parent, tree._contents.get(7));
     tap.equal(tree._contents.get(5).left, tree.NIL);
     tap.equal(tree._contents.get(5).right, tree.NIL);
+    tap.equal(tree._contents.get(7).parent, tree._contents.get(8));
+    tap.equal(tree._contents.get(7).left, tree._contents.get(5));
+    tap.equal(tree._contents.get(7).right, tree.NIL);
+    tap.equal(tree._contents.get(8).parent, tree._contents.get(2));
+    tap.equal(tree._contents.get(8).left, tree._contents.get(7));
+    tap.equal(tree._contents.get(8).right, tree.NIL);
+    tap.equal(tree._contents.get(11).parent, tree.NIL);
+    tap.equal(tree._contents.get(11).left, tree._contents.get(2));
+    tap.equal(tree._contents.get(11).right, tree._contents.get(14));
+    tap.equal(tree._contents.get(14).parent, tree._contents.get(11));
+    tap.equal(tree._contents.get(14).left, tree.NIL);
+    tap.equal(tree._contents.get(14).right, tree._contents.get(15));
+    tap.equal(tree._contents.get(15).parent, tree._contents.get(14));
+    tap.equal(tree._contents.get(15).left, tree.NIL);
+    tap.equal(tree._contents.get(15).right, tree.NIL);
 });
 // _leftRotate test end
 
@@ -437,3 +437,6 @@ tap.test('_rightRotate case: x is x.parent.left, y.right is NIL', async tap => {
     tap.equal(tree._contents.get(9).right, tree.NIL);
 });
 // _rightRotate test end
+
+// _insert and _insertFixup test start
+// _insert and _insertFixup test end
